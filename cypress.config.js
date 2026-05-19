3409bdf785f8b79413f9736e95ec00d8f1d4ede8
Jorre Van Dyck <r1036765@student.thomasmore.be>
2026-05-19 16:34:22 +0200

    Connect Cypress project to Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "tr9edc",
+
   e2e: {
-    baseUrl: "https://r1036765-realbeans.myshopify.com/",
+    baseUrl: "https://r1036765-realbeans.myshopify.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
